ddbdfa807c0dacbe45cfe2f28476a85c3d34763b
Dizi <58800976+DiziASP@users.noreply.github.com>
2021-11-09 18:39:30 +0700

    Bruh

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from "next";
 import Header from "../src/components/main/CustomHead";
 import Layout from "../src/components/main/Layout";
 import TopMenu from "../src/components/main/TopMenu";
-import IconPicture from "../src/components/other/IconPicture";
+import CandidateBox, {
+  CandidateChosen,
+} from "../src/components/other/CandidateBox";
 import AgendaComp from "../src/components/other/AgendaComp";
 import React from "react";
 
@@ -24,28 +26,11 @@ const Home: NextPage = () => {
             </div>
 
             {/* Candidate List */}
-            <div className="flex flex-col justify-center py-5 space-y-10">
-              <p className="text-lg font-light tracking-wide text-white ">
-                Berlangsung dari __/__/__ s.d. __/__/__
-              </p>
-
-              <div className="flex flex-wrap justify-start overflow-hidden">
-                <IconPicture name="lorem" />
-                <IconPicture name="ipsum" />
-                <IconPicture name="dolor" />
-                <IconPicture name="amet" />
-                <IconPicture name="hehe" />
-                <IconPicture name="boah" />
-              </div>
-            </div>
+            <CandidateChosen />
           </div>
 
-          <button className="z-10 p-3 pl-6 pr-6 -mt-6 text-xl font-bold transition-colors duration-700 transform bg-white rounded-lg shadow-md mx-14 md:mx-36 text-primary hover:bg-primary hover:text-white">
-            Pilih Ketua Sekarang!
-          </button>
-
           {/* Agenda */}
-          <div className="z-0 flex-grow px-6 py-6 -mt-6 space-y-6 bg-white">
+          <div className="z-0 flex-grow px-6 py-6 -mt-12 space-y-6 bg-white">
             <h1 className="pt-4 text-4xl font-bold">Agenda</h1>
             <div className="space-y-4">
               <AgendaComp content="Pemilihan dibuka" date="32 Feb" />
--- src/components/other/CandidateBox.tsx
+++ src/components/other/CandidateBox.tsx
@@ -1,12 +1,38 @@
 import React from "react";
+import IconPicture from "./IconPicture";
 
-const CandidateBox: React.FC<{
-  name: string;
-}> = ({ name }) => {
+const CandidateBox: React.FC = () => {
   return (
-    <div className="flex flex-col mx-2 my-2 text-lg bg-white rounded-lg w-44 h-60">
-      <div className="w-full h-full bg-gray-400 rounded-t-lg"></div>
-      <h2 className="py-2 text-base font-semibold text-center ">{name}</h2>
+    <div>
+      <div className="flex flex-col justify-center py-5 space-y-10">
+        <p className="text-lg font-light tracking-wide text-white ">
+          Berlangsung dari __/__/__ s.d. __/__/__
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-start overflow-hidden">
+        <IconPicture name="lorem" />
+        <IconPicture name="ipsum" />
+        <IconPicture name="dolor" />
+        <IconPicture name="amet" />
+        <IconPicture name="hehe" />
+        <IconPicture name="boah" />
+      </div>
+      <div className="flex flex-row justify-center">
+        <button className="z-10 p-3 pl-6 pr-6 text-xl font-bold transition-colors duration-700 transform bg-white rounded-lg shadow-md text-primary hover:bg-primary hover:text-white">
+          Pilih Ketua Sekarang!
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const CandidateChosen: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-lg text-white">Kamu telah memilih: </h1>
+      <div className="z-10 flex flex-row justify-center p-3 pl-6 pr-6">
+        <IconPicture name="Nuha" />
+      </div>
     </div>
   );
 };
