1eaf0bc77ea33439b428429331807062193c37fc
arsaizdihar <arsadihar@gmail.como>
2021-11-18 09:05:54 +0700

    change votingdate

--- src/constants/votingDate.ts
+++ src/constants/votingDate.ts
@@ -1,2 +1,2 @@
-export const votingDate = new Date("Nov 26 2021 00:00:00 GMT+0700");
+export const votingDate = new Date("Nov 18 2021 00:00:00 GMT+0700");
 export const votingEnd = new Date("Nov 26 2021 12:00:00 GMT+0700");
